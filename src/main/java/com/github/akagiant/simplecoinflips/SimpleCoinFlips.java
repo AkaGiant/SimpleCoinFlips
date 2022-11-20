1a880c3557a14f1d17f13ee83a7a8b45e31e9063
AkaGiant <akagiant01@gmail.com>
2022-11-20 20:35:13 +0000

    Slight Clean up to primary file and dependency management.

--- src/main/java/com/github/akagiant/simplecoinflips/SimpleCoinFlips.java
+++ src/main/java/com/github/akagiant/simplecoinflips/SimpleCoinFlips.java
@@ -1,20 +1,15 @@
 package com.github.akagiant.simplecoinflips;
 
-import com.github.akagiant.simplecoinflips.dependencies.PlaceholderManager;
 import com.github.akagiant.simplecoinflips.util.Logger;
+import com.github.akagiant.simplecoinflips.util.Util;
 import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPIConfig;
-import dev.jorel.commandapi.RegisteredCommand;
 import lombok.Getter;
+import lombok.Setter;
 import me.akagiant.giantapi.util.Config;
 import net.milkbowl.vault.economy.Economy;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public final class SimpleCoinFlips extends JavaPlugin {
 
@@ -23,6 +18,7 @@ public final class SimpleCoinFlips extends JavaPlugin {
 	public static Config config;
 	public static boolean hasPAPI = false;
 
+	@Setter
 	private static Economy economy = null;
 
 	@Override
@@ -45,12 +41,12 @@ public final class SimpleCoinFlips extends JavaPlugin {
 		registerCommands();
 		registerEvents();
 
-		Logger.toConsole("&fCommands Loaded (&a" + (CommandAPI.getRegisteredCommands().size())+ "&f) &8| &fAliases: (&a" + getCommandAliasesCount() + "&f)");
-		Logger.toConsole("&fPermissions Loaded (&a" + (getPermissionsCount())+ "&f)");
+		Logger.toConsole("&fCommands Loaded (&a" + (CommandAPI.getRegisteredCommands().size())+ "&f) &8| &fAliases: (&a" + Util.getCommandAliasesCount() + "&f)");
+		Logger.toConsole("&fPermissions Loaded (&a" + (Util.getPermissionsCount())+ "&f)");
 
-		findDepends();
-		findSoftDepends();
-		registerDependencies();
+		Util.findDepends(plugin);
+		Util.findSoftDepends(plugin);
+		Util.registerDependencies(plugin);
 
 		Logger.toConsole("&m————————————————————————————————————");
 		Logger.toConsole("&ahas been Enabled");
@@ -77,71 +73,5 @@ public final class SimpleCoinFlips extends JavaPlugin {
 	private void registerEvents() {
 	}
 
-	private int getPermissionsCount() {
-		int amount = 0;
-		for (RegisteredCommand cmd : CommandAPI.getRegisteredCommands()) {
-			if (cmd.permission() != null) { amount += 1; }
-		}
-		return amount;
-	}
 
-	private int getCommandAliasesCount() {
-		int amount = 0;
-		for (RegisteredCommand cmd : CommandAPI.getRegisteredCommands()) {
-			if (cmd.aliases() != null) { amount += cmd.aliases().length; }
-		}
-		return amount;
-	}
-
-	private void findDepends() {
-		if (plugin.getDescription().getDepend().isEmpty()) { return; }
-		Logger.toConsole("&m——————————&r &fDependencies &m&8————————");
-		Logger.toConsole("&fLooking for &a" + plugin.getDescription().getSoftDepend().size() + " &fSoft Dependencies");
-
-		List<String> found = new ArrayList<>();
-		List<String> missing = new ArrayList<>();
-		for (String dependency : plugin.getDescription().getDepend()) {
-			if (Bukkit.getServer().getPluginManager().getPlugin(dependency) == null) missing.add(dependency);
-			else found.add(dependency);
-		}
-
-		if (!found.isEmpty()) Logger.toConsole("&fFound &8| &a" + String.join("&8, &a", found));
-		if (!missing.isEmpty()) Logger.toConsole("&fMissing &8| &c" + String.join("&8, &c", missing));
-
-	}
-
-	private void findSoftDepends() {
-		if (plugin.getDescription().getSoftDepend().isEmpty()) { return; }
-		Logger.toConsole("&m——————————&r &fSoft Dependencies &m&8————————");
-		Logger.toConsole("&fLooking for &a" + plugin.getDescription().getSoftDepend().size() + " &fSoft Dependencies");
-
-		List<String> found = new ArrayList<>();
-		List<String> missing = new ArrayList<>();
-		for (String dependency : plugin.getDescription().getSoftDepend()) {
-			if (Bukkit.getServer().getPluginManager().getPlugin(dependency) == null) missing.add(dependency);
-			else found.add(dependency);
-		}
-
-		if (!found.isEmpty()) Logger.toConsole("&fFound &8| &a" + String.join("&8, &a", found));
-		if (!missing.isEmpty()) Logger.toConsole("&fMissing &8| &c" + String.join("&8, &c", missing));
-
-	}
-
-	private void registerDependencies() {
-
-		if (Bukkit.getServer().getPluginManager().getPlugin("Vault") == null) {
-			Logger.severe("Vault Not Found");
-			getServer().getPluginManager().disablePlugin(this);
-		} else {
-			if (getServer().getPluginManager().getPlugin("Vault") == null) { return; }
-			RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-			if (rsp == null) { return; }
-			economy = rsp.getProvider();
-		}
-
-		if (Bukkit.getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
-			new PlaceholderManager().register();
-			hasPAPI = true;
-		}
-	}
 }
---
+++ src/main/java/com/github/akagiant/simplecoinflips/util/Util.java
@@ -0,0 +1,89 @@
+package com.github.akagiant.simplecoinflips.util;
+
+import com.github.akagiant.simplecoinflips.SimpleCoinFlips;
+import com.github.akagiant.simplecoinflips.dependencies.PlaceholderManager;
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.RegisteredCommand;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.bukkit.Bukkit.getServer;
+
+public class Util {
+
+	private Util() {
+		//no instance
+	}
+	
+	public static int getPermissionsCount() {
+		int amount = 0;
+		for (RegisteredCommand cmd : CommandAPI.getRegisteredCommands()) {
+			if (cmd.permission() != null) { amount += 1; }
+		}
+		return amount;
+	}
+
+	public static int getCommandAliasesCount() {
+		int amount = 0;
+		for (RegisteredCommand cmd : CommandAPI.getRegisteredCommands()) {
+			if (cmd.aliases() != null) { amount += cmd.aliases().length; }
+		}
+		return amount;
+	}
+
+	public static void findDepends(Plugin plugin) {
+		if (plugin.getDescription().getDepend().isEmpty()) { return; }
+		Logger.toConsole("&m——————————&r &fDependencies &m&8————————");
+		Logger.toConsole("&fLooking for &a" + plugin.getDescription().getSoftDepend().size() + " &fSoft Dependencies");
+
+		List<String> found = new ArrayList<>();
+		List<String> missing = new ArrayList<>();
+		for (String dependency : plugin.getDescription().getDepend()) {
+			if (getServer().getPluginManager().getPlugin(dependency) == null) missing.add(dependency);
+			else found.add(dependency);
+		}
+
+		if (!found.isEmpty()) Logger.toConsole("&fFound &8| &a" + String.join("&8, &a", found));
+		if (!missing.isEmpty()) Logger.toConsole("&fMissing &8| &c" + String.join("&8, &c", missing));
+	}
+
+	public static void findSoftDepends(Plugin plugin) {
+		if (plugin.getDescription().getSoftDepend().isEmpty()) { return; }
+		Logger.toConsole("&m——————————&r &fSoft Dependencies &m&8————————");
+		Logger.toConsole("&fLooking for &a" + plugin.getDescription().getSoftDepend().size() + " &fSoft Dependencies");
+
+		List<String> found = new ArrayList<>();
+		List<String> missing = new ArrayList<>();
+		for (String dependency : plugin.getDescription().getSoftDepend()) {
+			if (getServer().getPluginManager().getPlugin(dependency) == null) missing.add(dependency);
+			else found.add(dependency);
+		}
+
+		if (!found.isEmpty()) Logger.toConsole("&fFound &8| &a" + String.join("&8, &a", found));
+		if (!missing.isEmpty()) Logger.toConsole("&fMissing &8| &c" + String.join("&8, &c", missing));
+	}
+
+	public static void registerDependencies(Plugin plugin) {
+
+		if (getServer().getPluginManager().getPlugin("Vault") == null) {
+			Logger.severe("Vault Not Found");
+			getServer().getPluginManager().disablePlugin(plugin);
+		} else {
+			if (getServer().getPluginManager().getPlugin("Vault") == null) { return; }
+			RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+			if (rsp == null) { return; }
+			SimpleCoinFlips.setEconomy(rsp.getProvider());
+		}
+
+		if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+			new PlaceholderManager().register();
+			SimpleCoinFlips.hasPAPI = true;
+		}
+	}
+	
+}
